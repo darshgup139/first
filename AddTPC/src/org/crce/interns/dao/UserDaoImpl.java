463e599571e7785edd5e57cb5d11e2b60e0592f2
Adarsh <adarshgupta139@gmail.com>
2016-03-12 21:13:55 +0530

    Update UserDaoImpl.java
    working insert
    delete coming soon

--- AddTPC/src/org/crce/interns/dao/UserDaoImpl.java
+++ AddTPC/src/org/crce/interns/dao/UserDaoImpl.java
@@ -18,12 +18,8 @@ public class UserDaoImpl implements UserDao{
 	@Override
 	public void insertUser(User user) {
 		// TODO Auto-generated method stub
-		//entityManager.persist(user);
-	//	List l=(List) entityManager.createQuery("");
-		// from the user.username check if that entry is presetn in db
-		// check the role of selected tuple
-		// now if the role == studetn then new_role == studenttpc
-		// else faculty tpc
+		//user.setUserRole("Student");
+		//System.out.println(user.getUserRole());
 		entityManager.merge(user);
 	}
 	@Override
@@ -32,13 +28,19 @@ public class UserDaoImpl implements UserDao{
 		return entityManager.createQuery("select u from User u", User.class).getResultList();
 	}
 	
-	@Override
+/*	@Override
 	public User getUser(User checkUser) {
 		// TODO Auto-generated method stub
-		String username=checkUser.getUsername();
-		User user = (User)entityManager.createQuery("select u from User u where where u.userName = :n").setParameter("n", username), User.class);
+		User user =entityManager.createQuery("select u from User u where where u.userName = :n").setParameter("n", username), User.class);
 		return user;
 	}	
+	*/
+	public User getUser(User checkUser) {
+  		// TODO Auto-generated method stub
+ 		String userName=checkUser.getUserName();
+  		User user = (User)entityManager.createQuery("select u from User u where u.userName = :n").setParameter("n", userName).getSingleResult();
+ 		return user;
+  	}	
 	
 	@Override
 	public void deleteUser(String userName) {
