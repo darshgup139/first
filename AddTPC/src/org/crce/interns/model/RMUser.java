768cdc8ab370a35c2314f0f9ad83cc144746a6bc
Adarsh Gupta <adarshgupta139@gmail.com>
2016-04-10 13:05:20 +0530

    Update RMUser.java

--- AddTPC/src/org/crce/interns/model/RMUser.java
+++ AddTPC/src/org/crce/interns/model/RMUser.java
@@ -8,8 +8,8 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-//@Table(name = "role_function_schema.role_master")
-@Table(name = "sample.role_master")
+@Table(name = "role_function_schema.role_master")
+//@Table(name = "sample.role_master")
 public class RMUser implements Serializable {
 	@Id
 	@Column(name = "role_id", nullable = false)
